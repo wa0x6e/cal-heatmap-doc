0ab8ac0de875365a317c0feaa3ad68eeb32bd228
wa0x6e <wan@wanki.moe>
2023-01-23 18:27:37 +0400

    feat: init basic docusaurus project

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/wan/Websites/Github/cal-heatmap-doc/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/wan/Websites/Github/cal-heatmap-doc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/wan/Websites/Github/cal-heatmap-doc/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/wan/Websites/Github/cal-heatmap-doc/src/css/custom.css'),
+];

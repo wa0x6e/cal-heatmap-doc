e94bb91ac5966a705590295564bf9698d2d8cadf
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-01-27 16:17:56 +0800

    fix: fix edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,14 +41,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/wa0x6e/cal-heatmap-doc/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/wa0x6e/cal-heatmap-doc/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
